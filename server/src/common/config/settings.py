7fe2298c09446f7db3830cbe81500383b8936e75
Amanbig <amanpreetsinghjhiwant@outlook.com>
2026-02-09 23:44:40 +0530

    added ruff linting

--- server/src/common/config/settings.py
+++ server/src/common/config/settings.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DATABASE_URL : str
     APP_ENV: str
+    JWT_SECRET: str
+    
     class Config:
         env_file = ".env"
         
