631392555707675dd643dd5fdbdd43599a017b24
Amanbig <amanpreetsinghjhiwant@outlook.com>
2026-02-10 23:56:16 +0530

    Created repository and models

--- server/src/common/db/session.py
+++ server/src/common/db/session.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import sessionmaker
 
 engine = create_engine("sqllite:///mlcore_db")
 
-
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
--- server/src/modules/user/store/model.py
+++ server/src/modules/user/store/model.py
@@ -1,1 +1,14 @@
-from 
+from sqlalchemy import UUID, String
+from sqlalchemy.schema import Column
+from src.common.db.base import Base
+from src.common.db.tables import Tables
+
+class User(Base):
+    __tablename__ = Tables.USERS
+    
+    id: Column[UUID]
+    Username: Column[String]
+    Email: Column[String]
+    phone: Column[String]
+    
+    password_hash: Column[String]
