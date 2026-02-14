7ee75ae9dc2ee0d5fce7c67c7945f75f7df37f54
Amanbig <amanpreetsinghjhiwant@outlook.com>
2026-02-14 12:02:39 +0530

    updated the base repository

--- server/src/common/repository/base.py
+++ server/src/common/repository/base.py
@@ -1,125 +1,111 @@
-from os import error
-from pydantic.main import BaseModel
-from sqlalchemy.orm.session import Session
-from typing import Any, List, Union, Optional, Generic,TypeVar
-from sqlalchemy.orm.strategy_options import joinedload
+from typing import Any, List, Union, Optional, Generic, TypeVar, Type
+from pydantic import BaseModel
+from sqlalchemy.orm import Session, joinedload
+from sqlalchemy.exc import SQLAlchemyError
 
 T = TypeVar("T")
 
 class BaseRepository(Generic[T]):
-    def __init__(self, model):
+    def __init__(self, model: Type[T]):
         self.model = model
-        
+
     def get(
         self,
         db: Session,
-        filters: Union[BaseModel,dict,None] = None,
-        load_relationships:Optional[list[str]] = None
-    )->List[T]:
+        filters: Union[BaseModel, dict, None] = None,
+        load_relationships: Optional[List[str]] = None
+    ) -> List[T]:
+
         query = db.query(self.model)
-        
-        if load_relationships:
-            for rel in load_relationships:
-                if hasattr(self.model,rel):
-                    query = query.options(joinedload(getattr(self.model,rel)))
-                    
-        
-        if filters:
-            filter_data = (
-                filters.model_dump(exclude_unset=True)
-                if isinstance(filters,BaseModel)
-                else filters
-            )
-            for field, value in filter_data.items():
-                if value is not None and hasattr(self.model,field):
-                    query = query.filter(getattr(self.model,field) == value)
-        
-        return query.all()
-        
-    
-    def create(
-        self,
-        db:Session,
-        obj_in: Any,
-        commit: bool = True
-    )->T:
-        if isinstance(obj_in,self.model):
-            db_obj = obj_in
-        elif hasattr(obj_in,"model_dump"):
-            db_obj = self.model(**obj_in.model_dump())
-        else:
-            db_obj = self.model(**obj_in)
-        
-        
-        db.add(db_obj)
-        
-        try:
-            if commit:
-                db.commit()
-                db.refresh(db_obj)
-        except error as e:
-            db.rollback()
-            print(e)
-            
-        return db_obj
-        
-    def update(
-        self,
-        db:Session,
-        db_obj: Any,
-        obj_in: Any,
-        commit: bool = True
-    )->T:
-        if hasattr(obj_in,"model_dump"):
-            updated_data = obj_in.model_dump(exclude_unset=True)
-        else:
-            updated_data = obj_in
-        
-        for field, value in updated_data.items():
-            if hasattr(db_obj,field):
-                setattr(db_obj,field, value)
-        
-        db.add(db_obj)
-        
-        try:
-            db.commit()
-            db.refresh(db_obj)
-        except error as e:
-            db.rollback()
-            print(e)
-        return db_obj
-    
-    def get_by_id(
-        self,
-        db:Session,
-        id: Any,
-        load_relationships:Optional[list[str]] = None
-    )->T | None:
-        query = db.query(self.model)
-        
+
         if load_relationships:
             for rel in load_relationships:
                 if hasattr(self.model, rel):
                     query = query.options(joinedload(getattr(self.model, rel)))
-                    
-        obj = query.filter(self.model.id == id).first()
-        
-        if not obj:
-            print("Error occured")
-        
-        return obj
-     
-    def delete(
+
+        if filters:
+            filter_data = (
+                filters.model_dump(exclude_unset=True)
+                if isinstance(filters, BaseModel)
+                else filters
+            )
+
+            for field, value in filter_data.items():
+                if value is not None and hasattr(self.model, field):
+                    query = query.filter(getattr(self.model, field) == value)
+
+        return query.all()
+
+    def create(self, db: Session, obj_in: Any, commit: bool = True) -> T:
+        if isinstance(obj_in, self.model):
+            db_obj = obj_in
+        elif hasattr(obj_in, "model_dump"):
+            db_obj = self.model(**obj_in.model_dump())
+        else:
+            db_obj = self.model(**obj_in)
+
+        db.add(db_obj)
+
+        try:
+            if commit:
+                db.commit()
+                db.refresh(db_obj)
+        except SQLAlchemyError:
+            db.rollback()
+            raise
+
+        return db_obj
+
+    def update(self, db: Session, db_obj: T, obj_in: Any, commit: bool = True) -> T:
+        updated_data = (
+            obj_in.model_dump(exclude_unset=True)
+            if hasattr(obj_in, "model_dump")
+            else obj_in
+        )
+
+        for field, value in updated_data.items():
+            if hasattr(db_obj, field):
+                setattr(db_obj, field, value)
+
+        db.add(db_obj)
+
+        try:
+            if commit:
+                db.commit()
+                db.refresh(db_obj)
+        except SQLAlchemyError:
+            db.rollback()
+            raise
+
+        return db_obj
+
+    def get_by_id(
         self,
-        db:Session,
+        db: Session,
         id: Any,
-    )->Optional[T]:
-        obj = self.get_by_id(db,id)
+        load_relationships: Optional[List[str]] = None
+    ) -> Optional[T]:
+
+        query = db.query(self.model)
+
+        if load_relationships:
+            for rel in load_relationships:
+                if hasattr(self.model, rel):
+                    query = query.options(joinedload(getattr(self.model, rel)))
+
+        return query.filter(getattr(self.model, id) == id).first()
+
+    def delete(self, db: Session, id: Any) -> Optional[T]:
+        obj = self.get_by_id(db, id)
+
+        if not obj:
+            return None
+
         try:
             db.delete(obj)
             db.commit()
-        except error as e:
+        except SQLAlchemyError:
             db.rollback()
-            print(e)
-        
+            raise
+
         return obj
