c5f43ce76a20468791972a34879adb185de457a0
Amanbig <amanpreetsinghjhiwant@outlook.com>
2026-02-22 13:04:07 +0530

    added initial shadcn ui components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { ThemeProvider } from './components/theme-provider'
+import { Toaster } from "./components/ui/sonner"
 
 function App({ children }: { children: React.ReactNode }) {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         {children}
+        <Toaster />
       </ThemeProvider>
     </>
   )
